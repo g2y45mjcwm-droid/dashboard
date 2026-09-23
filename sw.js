b6296ca73738bcfa94c7f8ea38e8ce9bdc606b4e
g2y45mjcwm-droid <g2y45mjcwm@privaterelay.appleid.com>
2026-09-23 17:07:31 +0800

    auto: 更新sw.js (09-23 17:07)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dashboard-v202609231602';
+const CACHE_NAME = 'dashboard-v202609231707';
 const urlsToCache = [
   'https://cdn.jsdelivr.net/npm/echarts@5.5.0/dist/echarts.min.js'
 ];
